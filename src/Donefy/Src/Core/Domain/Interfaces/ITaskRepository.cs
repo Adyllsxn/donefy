08414ddf78090c81d0c1dfbce73c916ce24955fc
Adyllson <domingosadyllsxn@gmail.com>
2025-09-21 18:26:54 +0100

    refactor(repository): segregate ITaskRepository into ITaskReadRepository and ITaskWriteRepository with Task<bool> return for write operations

--- src/Donefy/Src/Core/Domain/Interfaces/ITaskRepository.cs
+++ src/Donefy/Src/Core/Domain/Interfaces/ITaskRepository.cs
@@ -1,5 +1,16 @@
 namespace Donefy.Src.Core.Domain.Interfaces;
-public interface ITaskRepository
+public interface ITaskRepository : ITaskReadRepository, ITaskWriteRepository { }
+
+public interface ITaskReadRepository
 {
-    
+    Task<(List<TaskEntity> Tasks, int TotalCount)> GetAllAsync(PagedRequest request, CancellationToken token);
+    Task<TaskEntity?> GetByIdAsync(Guid taskId, CancellationToken token);
+}
+
+public interface ITaskWriteRepository  
+{
+    Task<bool> CreateAsync(TaskEntity task, CancellationToken token);
+    Task<bool> UpdateAsync(TaskEntity task, CancellationToken token);
+    Task<bool> DeleteAsync(Guid taskId, CancellationToken token);
+    Task<bool> UpdateStatusAsync(Guid taskId, EStatus newStatus, CancellationToken token);
 }
